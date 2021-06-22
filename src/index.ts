73811af618af8aac58f62fe5a2008f656d9ba5aa
ElifVerdaCodar <codarelifverda@gmail.com>
2021-06-22 20:31:25 +0300

    gridçiz yapıldı

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   var canvas = document.getElementById("canvas") as HTMLCanvasElement;
   var ctx = canvas.getContext("2d");
-  ctx.beginPath();
 
-  //35e kadar 20şer arttırarak dön. Böylece 700 pixellik alanda çiz
-  for (var i = 0; i < 35; i += 1) {
-    ctx.moveTo(0, i * 20);
-    ctx.lineTo(500, i * 20);
-  }
-
-  for (var j = 0; j < 25; j++) {
-    ctx.moveTo(20 * j, 0);
-    ctx.lineTo(20 * j, 700);
-  }
-
-  ctx.stroke();
+  gridCiz(ctx);
 
   //pozisyon
   var pozisyon = document.getElementById("position") as HTMLDivElement;
@@ -22,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     pozisyon.innerText = event.clientX + "," + event.clientY;
   });
 });
+
+function gridCiz(ctx: CanvasRenderingContext2D) {
+  ctx.beginPath();
+
+  //35e kadar 20şer arttırarak dön. Böylece 700 pixellik alanda çiz
+  for (var i = 0; i < 35; i += 1) {
+    ctx.moveTo(0, i * 20);
+    ctx.lineTo(500, i * 20);
+  }
+  //dikey çizgiler
+  for (var j = 0; j < 25; j++) {
+    ctx.moveTo(20 * j, 0);
+    ctx.lineTo(20 * j, 700);
+  }
+
+  ctx.stroke();
+}
